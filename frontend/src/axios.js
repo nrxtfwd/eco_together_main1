0b5749a80816cce243b12385580e14327f49c693
nrxtfwd <danishazhar2007@gmail.com>
2025-09-25 23:04:27 +0800

    https

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://eco-together-main1-2.onrender.com/api'
+const BASE_URL = 'https://eco-together-main1-2.onrender.com/api'
 
 const api = axios.create({
     baseURL : BASE_URL
