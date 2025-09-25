5f130d71ac772706bc26427730e2f2728285dc91
nrxtfwd <danishazhar2007@gmail.com>
2025-09-25 19:10:13 +0800

    hi

--- server/index.js
+++ server/index.js
@@ -6,8 +6,10 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import config from './config.js'
 import authRoutes from './routes/auth.js'
+import path from "path"
 
 const app = express();
+const __dirname = path.resolve()
 
 app.use(express.json());
 app.use(cors());
@@ -19,6 +21,11 @@ mongoose.connect(config.mongoURI, { useNewUrlParser: true,
 
 app.use('/api/auth', authRoutes);
 app.use('/api', projectRoutes)
+
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname,'../frontend','build','index.html'))
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
